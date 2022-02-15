da3a64062a28fa74a7a99117486c4f0728163bc2
LuckyBulgur <uguraydogan37@gmail.com>
2022-02-15 01:42:26 +0100

    update eslint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { AppProps } from 'next/app';
 import getConfig from 'next/config';
 import Head from 'next/head';
 import Router, { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import SideBar from '../components/SideBar';
@@ -15,24 +15,25 @@ const { publicRuntimeConfig } = getConfig()
 function MyApp({ Component, pageProps }: AppProps) {
 
   const paths = ["/", "/login", "/register", "/_error"];
-  const currentPath = useRouter().pathname;
   const router = useRouter();
+  const currentPath = router.pathname;
 
-  useEffect(() => {
-    const checkIsUserLoggedIn = async () => {
-      if (!paths.includes(currentPath)) {
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Authorization', 'Bearer ' + Cookies.get("access_token"));
-        const data = await fetch(`${publicRuntimeConfig.serverUrl}/user`, { headers });
-        const json = await data.json();
-        if (json.message == "Unauthorized") {
-          router.push('/login');
-        }
+  const checkIsUserLoggedIn = useCallback(async () => {
+    if (!paths.includes(currentPath)) {
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Authorization', 'Bearer ' + Cookies.get("access_token"));
+      const data = await fetch(`${publicRuntimeConfig.serverUrl}/user`, { headers });
+      const json = await data.json();
+      if (json.message == "Unauthorized") {
+        router.push('/login');
       }
     }
-    checkIsUserLoggedIn();
   }, []);
+
+  useEffect(() => {
+    checkIsUserLoggedIn();
+  }, [checkIsUserLoggedIn]);
 
   return (
     <>
